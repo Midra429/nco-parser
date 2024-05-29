3812cf6e746c4dbee0cdaaa13058d3eecc22c189
Midra <me@midra.me>
2024-05-29 17:46:10 +0900

    update-exports.cjs

---
+++ scripts/update-exports.cjs
@@ -0,0 +1,50 @@
+const fs = require('fs')
+const path = require('path')
+const { globSync } = require('glob')
+
+const packageJsonPath = path.resolve(__dirname, '../package.json')
+const distDir = path.resolve(__dirname, '../dist')
+
+const packageJson = require(packageJsonPath)
+const files = globSync(path.join(distDir, '/**/*.js'))
+
+packageJson.exports = {}
+
+files.forEach((file) => {
+  const dirSplited = path.relative(distDir, file).split('/')
+  const fileName = path.basename(file, '.js')
+
+  if (dirSplited.length === 1) {
+    const alias = fileName === 'index' ? '.' : `./${fileName}`
+
+    packageJson.exports[alias] = {
+      types: `./dist/${fileName}.d.ts`,
+      import: `./dist/${fileName}.js`,
+      default: `./dist/${fileName}.js`,
+    }
+  } else {
+    if (fileName === 'index') {
+      packageJson.exports['./*'] = {
+        types: './dist/*/index.d.ts',
+        import: './dist/*/index.js',
+        default: './dist/*/index.js',
+      }
+    } else {
+      packageJson.exports[`./${dirSplited[0]}/*`] = {
+        types: `./dist/${dirSplited[0]}/*.d.ts`,
+        import: `./dist/${dirSplited[0]}/*.js`,
+        default: `./dist/${dirSplited[0]}/*.js`,
+      }
+    }
+  }
+})
+
+// packageJson.exports['./types/*'] = {
+//   types: './dist/types/*.d.ts',
+// }
+
+packageJson.exports = Object.fromEntries(
+  Object.entries(packageJson.exports).sort(([a], [b]) => a.localeCompare(b))
+)
+
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
