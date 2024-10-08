512c60a9d5474994354b044a3184b2e3a043d0d0
Midra <me@midra.me>
2024-10-09 02:30:46 +0900

    直前に記号消してたら動かんやんけ

--- src/normalize/lib/remove/szbh.ts
+++ src/normalize/lib/remove/szbh.ts
@@ -1,6 +1,6 @@
 export const REGEXPS = [
   /コメント専?用(動画)?|szbh方式/gi,
-  /\s+(dvd|(blu[\s\-]?ray|ブル[ー\-]レイ))用|dvd\/(blu[\s\-]?ray|ブル[ー\-]レイ)用?/gi,
+  /\s+(dvd|(blu[\s\-]?ray|ブル[ー\-]レイ))用|dvd[\s\/](blu[\s\-]?ray|ブル[ー\-]レイ)用?/gi,
 ]
 
 export const szbh = (str: string): string => {
