ea17df39ebbf51e2f70dd657353199a9134508da
Midra <me@midra.me>
2025-08-06 21:56:21 +0900

    「シュタインズ・ゲート ゼロ」を英語表記にしない

--- src/normalize/lib/adjust/anime.ts
+++ src/normalize/lib/adjust/anime.ts
@@ -1,6 +1,6 @@
 export const anime = (str: string): string => {
   // シュタインズ・ゲート -> STEINS;GATE
-  str = str.replace(/シュタインズ[・\s]?ゲート/, 'STEINS;GATE')
+  str = str.replace(/シュタインズ[・\s]?ゲート(?!\s?ゼロ)/, 'STEINS;GATE')
 
   return str
 }
